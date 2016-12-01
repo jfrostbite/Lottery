2bddbb5894346e1d4f639c3119ddb80584748bf8
Kevin Feng <jf4044@163.com>
2016-12-01 23:04:11 +0800

    宝宝活动假如

--- Lottery/src/com/kevin/lottery/entity/PocoBean.java
+++ Lottery/src/com/kevin/lottery/entity/PocoBean.java
@@ -7,4 +7,12 @@ public class PocoBean {
     //{"change":1,"prize":1,"angle":18,"winkey":"1a68a4ca5c8069996731fe2a74e21894","record":1705}
     public String prize,winkey,record,ok,err,result,code,time,message;
     public boolean win,big;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setresult(String result) {
+        this.result = result;
+    }
 }
